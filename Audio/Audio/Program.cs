319478bb958c8969fa8827c1ddb2b1e481102470
Duc238 <92005265+Duc238@users.noreply.github.com>
2024-07-21 19:14:30 +0700

    Add files via upload

---
+++ Audio/Audio/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+using System.IO;
+using NAudio.Wave;
+namespace Audio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=Encoding.UTF8;
+            string connectionString = "Data Source=DESKTOP-460EDHU\\VOTANDUC;Initial Catalog=Audio;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT FileName, AudioData FROM AudioFiles WHERE ID = @ID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ID", 1);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string fileName = reader["FileName"].ToString();
+                            byte[] audioData = (byte[])reader["AudioData"];
+
+                            File.WriteAllBytes(fileName, audioData);
+                            Console.WriteLine($"File {fileName} đã được xuất thành công.");
+                            // Phát âm thanh
+                            PlayAudio(fileName);
+                        }
+                    }
+                }
+
+            }
+        }
+        static void PlayAudio(string fileName)
+        {
+            using (var audioFile = new AudioFileReader(fileName))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                Console.WriteLine("Đang phát âm thanh...");
+
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ Audio/Audio/bin/Debug/net8.0/Data/Data.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE Audio
+GO
+USE Audio
+GO
+CREATE TABLE AudioFiles (
+    ID INT PRIMARY KEY IDENTITY,
+    FileName NVARCHAR(255),
+    AudioData VARBINARY(MAX)
+)
+GO
+INSERT INTO AudioFiles (FileName, AudioData)
+VALUES ('AnhSangThienDuongOst-TuVi-4436811.mp3', 
+        (SELECT * FROM OPENROWSET(BULK N'E:\Lập trình\.NET core\Audio\Audio\bin\Debug\net8.0\Data\AnhSangThienDuongOst-TuVi-4436811.mp3', SINGLE_BLOB) AS AudioFile))
+GO
+SELECT FileName, AudioData
+FROM AudioFiles
+WHERE ID = 1
+GO
